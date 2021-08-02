70b8c20f6e7db710b62b9f6ff1b3c7e2d6bfab78
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-08-02 05:47:03 -0700

    Added champion get test, test is failing

--- __tests__/champion.test.js
+++ __tests__/champion.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-// import Champion from '../lib/models/Champion.js';
+import Champion from '../lib/models/Champion.js';
 
 describe('champion routes', () => {
   beforeEach(() => {
@@ -24,5 +24,19 @@ describe('champion routes', () => {
       id: '1',
       ...lux
     });
+  });
+
+  it('gets a champion by id via GET', async () => {
+    const ahri = await Champion.insert({ 
+      name: 'Ahri', 
+      role: 'mage', 
+      damage: 'ability power', 
+      difficulty: 2,
+      region: 'Ionia'
+    });
+
+    const res = await request(app).get(`/api/v1/champions/${ahri.id}`);
+
+    expect(res.body).toEqual(ahri);
   });
 });
