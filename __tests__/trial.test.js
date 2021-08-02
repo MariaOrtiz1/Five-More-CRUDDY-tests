3d38c8093c03b6d53a5f9c2578d1ec260f826c63
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-08-02 09:15:26 -0700

    Added trial getAll test, test is failing

--- __tests__/trial.test.js
+++ __tests__/trial.test.js
@@ -28,4 +28,32 @@ describe('trial routes', () => {
 
     expect(res.body).toEqual(tsukuyomi);
   });
+
+  it('gets all trials via GET', async () => {
+    const titania = await Trial.insert({
+      name: 'The Dancing Plague',
+      level: 73,
+      boss: 'Titania',
+      expansion: 'Shadowbringers',
+    });
+
+    const innocence = await Trial.insert({
+      name: 'The Crown of the Immaculate',
+      level: 79,
+      boss: 'Innocence',
+      expansion: 'Shadowbringers',
+    });
+
+    const hades = await Trial.insert({
+      name: 'The Dying Grasp',
+      level: 80,
+      boss: 'Hades',
+      expansion: 'Shadowbringers',
+    });
+
+    const res = await request(app)
+      .get('/api/v1/trials');
+
+    expect(res.body).toEqual([titania, innocence, hades]);
+  });
 });
